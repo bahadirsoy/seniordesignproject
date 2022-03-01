3579cf0975d92505f6ef3d396f6fd70340d4ec1a
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-03-01 11:03:57 +0300

    homepage page is getting all posts from database

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,17 +2,47 @@
 //import main components
 import React from 'react';
 import './homepage.styles.css';
+import { firestore } from '../../firebase/firebase.utils';
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 //import pages and components
 import Post from '../../components/post/post.component';
 import SharePostPanel from '../../components/sharepostpanel/sharepostpanel.component';
 
-const HomePage = () => (
-    <div>
-        <SharePostPanel />
-        <Post />
-        <Post />
-    </div>
-)
+class HomePage extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            posts: []
+        }
+    }
+
+    async componentDidMount(){
+        const q = query(collection(firestore, "posts"), where("displayName", "==", "furkan"));
+
+        const posts = [];
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+
+            posts.push(doc.data());
+
+        });
+        
+        this.setState({ posts: posts }, () => console.log(this.state.posts))
+
+    }
+
+    render(){
+        return(
+            <div>
+                <SharePostPanel />
+                <Post />
+                <Post />
+            </div>
+        )
+    }
+}
 
 export default HomePage;
