80cd080a89cc6189858b0f0a048367d1fe59f022
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-03-01 10:25:37 +0300

    sharepostpanel component is edited

--- src/components/sharepostpanel/sharepostpanel.component.jsx
+++ src/components/sharepostpanel/sharepostpanel.component.jsx
@@ -3,6 +3,13 @@
 import React from 'react';
 import './sharepostpanel.styles.css';
 
+//import FontAwesome Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+
+//import react strap components
+import { Container, Row } from 'react-bootstrap';
+
 
 class SharePostPanel extends React.Component{
     constructor(){
@@ -16,30 +23,34 @@ class SharePostPanel extends React.Component{
 
     render(){
         return(
-            <div className="panel-content panel-activity">
-                <form action="#" className="panel-activity__status">
-                    <textarea name="user_activity" placeholder="Share what you've been up to..." className="form-control"></textarea>
-                    <div className="actions">
-                        <div className="btn-group">
-                            <button type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Image">
-                                a
-                            </button>
-                            <button type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Video">
-                                b
-                            </button>
-                            <button type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Idea">
-                                c
-                            </button>
-                            <button type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Question">
-                                d
-                            </button>
-                        </div>
-                        <button type="submit" className="btn btn-sm btn-rounded btn-info">
-                            Post
-                        </button>
+            <Container>
+                <Row>
+                    <div className="panel-content panel-activity">
+                        <form action="#" className="panel-activity__status">
+                            <textarea name="user_activity" placeholder="Share what you've been up to..." className="form-control"></textarea>
+                            <div className="actions">
+                                <div className="btn-group">
+                                    <a type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Image">
+                                        <FontAwesomeIcon color='white' size="lg" icon={faImage} />
+                                    </a>
+                                    <a type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Video">
+                                        <FontAwesomeIcon color='white' size="lg" icon={faImage} />
+                                    </a>
+                                    <a type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Idea">
+                                        <FontAwesomeIcon color='white' size="lg" icon={faImage} />
+                                    </a>
+                                    <a type="button" className="btn-link" title="" data-toggle="tooltip" data-original-title="Post an Question">
+                                        <FontAwesomeIcon color='white' size="lg" icon={faImage} />
+                                    </a>
+                                </div>
+                                <button type="submit" className="btn btn-sm btn-rounded btn-info">
+                                    Post
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
+                </Row>
+            </Container>
         )
     }
 
